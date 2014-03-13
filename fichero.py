bca72677e8ebeb4d93327180f63b48d7acb077d1
usuario <usuario@debian.gonzalonazareno.org>
2014-03-13 10:49:18 +0000

    añadir activos_frecuencia y graves

---
+++ fichero.py
@@ -0,0 +1,18 @@
+from lxml import etree
+from suds.client import Client
+
+
+cliente = Client('http://www.infobustussam.com:9001/services/dinamica.asmx?wsdl', retxml=True)
+peticion = raw_input("Introduce una linea de Tussam: ")
+respuesta = cliente.service.GetStatusLinea(peticion)
+
+raiz = etree.fromstring(respuesta)
+raiz2 = raiz[0][0]
+
+prefijo = "{http://tempuri.org/}"
+
+activos = raiz2.find(prefijo+"GetStatusLineaResult/"+prefijo+"activos")
+frecuencia = raiz2.find(prefijo+"GetStatusLineaResult/"+prefijo+"frec_bien")
+graves = raiz2.find(prefijo+"GetStatusLineaResult/"+prefijo+"graves")
+
+print "En este momento hay %s activos , %s bien de frecuencia y %s incidencias" % (activos.text,frecuencia.text,graves.text)
