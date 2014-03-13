debdadc693831973930617614ee5fd43006da4cf
usuario <usuario@debian.gonzalonazareno.org>
2014-03-13 10:59:22 +0000

    corregir clear

--- fichero.py
+++ fichero.py
@@ -2,7 +2,7 @@ from lxml import etree
 from suds.client import Client
 import os
 
-
+os.system("clear")
 cliente = Client('http://www.infobustussam.com:9001/services/dinamica.asmx?wsdl', retxml=True)
 peticion = raw_input("Introduce una linea de Tussam: ")
 respuesta = cliente.service.GetStatusLinea(peticion)
@@ -16,6 +16,7 @@ activos = raiz2.find(prefijo+"GetStatusLineaResult/"+prefijo+"activos")
 frecuencia = raiz2.find(prefijo+"GetStatusLineaResult/"+prefijo+"frec_bien")
 graves = raiz2.find(prefijo+"GetStatusLineaResult/"+prefijo+"graves")
 
-os.system("clear")
 
-print "En este momento hay %s activos , %s bien de frecuencia y %s incidencias" % (activos.text,frecuencia.text,graves.text)
+
+print """En este momento hay %s activos, %s bien de frecuencia y %s incidencias
+""" % (activos.text,frecuencia.text,graves.text)
