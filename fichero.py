ab260bec33b352b035f4b263406144ccfc3105c0
usuario <usuario@debian.gonzalonazareno.org>
2014-03-13 10:50:06 +0000

    añadir clear

--- fichero.py
+++ fichero.py
@@ -1,5 +1,6 @@
 from lxml import etree
 from suds.client import Client
+import os
 
 
 cliente = Client('http://www.infobustussam.com:9001/services/dinamica.asmx?wsdl', retxml=True)
@@ -14,5 +15,7 @@ prefijo = "{http://tempuri.org/}"
 activos = raiz2.find(prefijo+"GetStatusLineaResult/"+prefijo+"activos")
 frecuencia = raiz2.find(prefijo+"GetStatusLineaResult/"+prefijo+"frec_bien")
 graves = raiz2.find(prefijo+"GetStatusLineaResult/"+prefijo+"graves")
+
+os.system("clear")
 
 print "En este momento hay %s activos , %s bien de frecuencia y %s incidencias" % (activos.text,frecuencia.text,graves.text)
